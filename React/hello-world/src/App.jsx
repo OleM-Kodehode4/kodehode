451ee7470817da1b1c73443bd5a4e7f42010b4f8
OleM-Kodehode4 <olemathias.kodehode@gmail.com>
2025-01-07 09:38:46 +0100

    react

--- React/hello-world/src/App.jsx
+++ React/hello-world/src/App.jsx
@@ -4,10 +4,10 @@ import Header2 from './components/header2';
 function App() {
 
   return (
-    <>
+    <div>
     <Header />    
     <Header2 text="Hei på deg" />    
-    </>
+    </div>
   );
 }
 
