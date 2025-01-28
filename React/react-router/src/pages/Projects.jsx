bd2d80548a6f6ad00d0a7af55d4e246e714b0a72
OleM-Kodehode4 <olemathias.kodehode@gmail.com>
2025-01-28 09:11:57 +0100

    React-Router

---
+++ React/react-router/src/pages/Projects.jsx
@@ -0,0 +1,23 @@
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
+const mockProjects = {
+    'prosjekt1': {
+        title: 'Prosjekt 1',
+        description: 'Om prosjektet...',
+    },
+    'prosjekt2': {
+        title: 'Prosjekt 2',
+        description: 'Om prosjektet...',
+    },
+    'prosjekt3': {
+        title: 'Prosjekt 3',
+        description: 'Om prosjektet...',
+    },
+    'prosjekt4': {
+        title: 'Prosjekt 4',
+        description: 'Om prosjektet...',
+    }
+}
+
