8da360de497b739d9e18955dc7352edbee3066e6
OleM-Kodehode4 <olemathias.kodehode@gmail.com>
2025-01-30 10:49:52 +0100

    Refactor App component to implement routing and add initial page structure with theme context

--- React/fjell-kino/src/App.jsx
+++ React/fjell-kino/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import  useState  from "react";
+import  Navbar  from "./components/Navbar";
+import  Footer  from "./components/Footer";
+import  Home  from "./pages/Home";
+import  Movies  from "./pages/Movies";
+import  UpcomingMovies  from "./pages/UpcomingMovies";
+import  About  from "./pages/About";
+import  Contact  from "./pages/Contact";
+import  Booking  from "./pages/Booking";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/upcoming-movies" element={<UpcomingMovies />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/booking" element={<Booking />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ React/fjell-kino/src/pages/index.jsx
@@ -0,0 +1,6 @@
+// import { Home } from "./pages/Home";
+// import { Movies } from "./pages/Movies";
+// import { UpcomingMovies } from "./pages/UpcomingMovies";
+// import { About } from "./pages/About";
+// import { Contact } from "./pages/Contact";
+// import { Booking } from "./pages/Booking";
